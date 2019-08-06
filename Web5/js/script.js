4a5f425d5484bd15dcccf3d0490c9a054d1dd0a0
Vitalii <Vitaliy.Pidkivka@gmail.com>
2019-08-06 14:54:43 +0300

    2

--- Web5/js/script.js
+++ Web5/js/script.js
@@ -103,13 +103,15 @@ function clear(id) {
 };
 
 function backSpace() {
-    let localDecimalMemory = display[0].innerHTML || display[1].innerHTML;
-    if (display[0].innerHTML.length != '1' || display[1].innerHTML.length != '1') {
+    let localDecimalMemory = display[0].innerHTML;
+    if (display[1].innerHTML.length != '1') {
         display[0].innerHTML = localDecimalMemory.substring(0, localDecimalMemory.length - 1);
         display[1].innerHTML = localDecimalMemory.substring(0, localDecimalMemory.length - 1);
-    } else if (display.innerHTML.length = '1') {
+    } else if (display[0].innerHTML.length == '1') {
         localDecimalMemory == '0';
         display[0].innerHTML = localDecimalMemory;
+    } else if (display[1].innerHTML.length == '1') {
+        localDecimalMemory == '0';
         display[1].innerHTML = localDecimalMemory;
     }
 }
