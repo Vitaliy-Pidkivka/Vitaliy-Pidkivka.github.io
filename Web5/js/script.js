2b0eba51fe4b3abfc186934fedd351d3a580e0c1
Vitalii <Vitaliy.Pidkivka@gmail.com>
2019-08-06 14:26:48 +0300

    2

--- Web5/js/script.js
+++ Web5/js/script.js
@@ -4,11 +4,10 @@ let numbersBtn = $('.numbers'),
     clearBtns = $('.clear_btn'),
     backBtn = $('#back'),
     resultBtn = $('#result'),
-    display = document.getElementById('display'),
+    display = $('.display'),
     MemoryCurrentNumber = "0",
     MemoryNewNumber = false,
     MemoryPendingOperation = '';
-
 
 for (let i = 0; i < numbersBtn.length; i++) {
     let number = numbersBtn[i];
@@ -34,22 +33,26 @@ $('#back').on('click', backSpace)
 
 function numberPress(number) {
     if (MemoryNewNumber) {
-        display.innerHTML = number;
+        display[0].innerHTML = number;
+        display[1].innerHTML = number;
         MemoryNewNumber = false;
     } else {
-        if (display.innerHTML === '0') {
-            display.innerHTML = number;
+        if (display[0].innerHTML === '0' || display[1].innerHTML === '0') {
+            display[0].innerHTML = number;
+            display[1].innerHTML = number;
         } else {
-            display.innerHTML += number;
+            display[0].innerHTML += number;
+            display[1].innerHTML += number;
         }
     }
 
 };
 
 function operations(op) {
-    let lolacOperationMemory = display.innerHTML;
+    let lolacOperationMemory = display[0].innerHTML || display[1].innerHTML;
     if (MemoryNewNumber && MemoryPendingOperation !== '=') {
-        display.innerHTML = MemoryCurrentNumber;
+        display[0].innerHTML = MemoryCurrentNumber;
+        display[1].innerHTML = MemoryCurrentNumber;
     } else {
         MemoryNewNumber = true;
         if (MemoryPendingOperation === '+') {
@@ -64,13 +67,14 @@ function operations(op) {
             MemoryCurrentNumber = parseFloat(lolacOperationMemory);
         }
 
-        display.innerHTML = MemoryCurrentNumber;
+        display[0].innerHTML = MemoryCurrentNumber;
+        display[1].innerHTML = MemoryCurrentNumber;
         MemoryPendingOperation = op;
     }
 };
 
 function decimal() {
-    let localDecimalMemory = display.innerHTML;
+    let localDecimalMemory = display[0].innerHTML || display[1].innerHTML;
     if (MemoryNewNumber) {
         localDecimalMemory = '0.';
         MemoryNewNumber = false;
@@ -79,16 +83,19 @@ function decimal() {
             localDecimalMemory += '.';
         };
     };
-    display.innerHTML = localDecimalMemory;
+    display[0].innerHTML = localDecimalMemory;
+    display[1].innerHTML = localDecimalMemory;
 };
 
 
 function clear(id) {
     if (id === 'ce') {
-        display.innerHTML = '0';
+        display[0].innerHTML = '0';
+        display[1].innerHTML = '0';
         MemoryNewNumber = true;
     } else if (id === 'c') {
-        display.innerHTML = '0';
+        display[0].innerHTML = '0';
+        display[1].innerHTML = '0';
         MemoryNewNumber = true;
         MemoryCurrentNumber = 0;
         MemoryPendingOperation = '';
@@ -96,11 +103,13 @@ function clear(id) {
 };
 
 function backSpace() {
-    let localDecimalMemory = display.innerHTML;
-    if (display.innerHTML.length != '1') {
-        display.innerHTML = localDecimalMemory.substring(0, localDecimalMemory.length - 1);
+    let localDecimalMemory = display[0].innerHTML || display[1].innerHTML;
+    if (display[0].innerHTML.length != '1' || display[1].innerHTML.length != '1') {
+        display[0].innerHTML = localDecimalMemory.substring(0, localDecimalMemory.length - 1);
+        display[1].innerHTML = localDecimalMemory.substring(0, localDecimalMemory.length - 1);
     } else if (display.innerHTML.length = '1') {
         localDecimalMemory == '0';
-        display.innerHTML = localDecimalMemory;
+        display[0].innerHTML = localDecimalMemory;
+        display[1].innerHTML = localDecimalMemory;
     }
 }
