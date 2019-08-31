203b78bd2c3df5db7f5457fe939ac18f3e2f263a
Vitalii <Vitaliy.Pidkivka@gmail.com>
2019-08-31 17:12:59 +0300

    added photo to resume

--- Resume/js/script.js
+++ Resume/js/script.js
@@ -29,34 +29,48 @@ function navMenu() {
 
 
 function navContact() {
+    $(document).ready(function () {
+        $('#contact, #contactMe').on('click', () => {
+            if ($(window).width() < 575) {
+                $('.aside-contact').css({
+                    'margin-top': '50px',
+                    'background-color': 'yellow',
+                    'border': '5px solid rgb(47, 165, 159)',
+                    'border-radius': '12px',
+                });
 
-    $('#contact, #contactMe' ).on('click', () => {
-        $('.aside-contact').addClass('focus');
-        let intervalFirstId = setInterval(() => {
-            $('.aside-contact').css({
-                'transform': 'scale(1.35)',
-                'margin-right': '50px',
-                'margin-top': '50px',
-                'background-color': 'yellow',
-            });
-        }, 10);
-        setTimeout(() => {
-            clearInterval(intervalFirstId);
-            $('.aside-contact').css({
-                'transform': 'scale(1)',
-                'margin-right': '0px',
-                'margin-top': '0px',
-                'background-color': 'rgba(252, 252, 252, 0.932)',
-            });
-            setTimeout(() => {
-                $('.aside-contact').removeClass('focus');
-            }, 450);
+                setTimeout(() => {
 
-        }, 3400);
+                    $('.aside-contact').css({
+                        'margin-top': '0px',
+                        'background-color': 'rgba(252, 252, 252, 0.932)',
+                        'border': 'none',
+                    });
+                }, 3400);
+            } else{
+                $('.aside-contact').css({
+                    'transform': 'scale(1.2)',
+                    'margin-top': '50px',
+                    'background-color': 'yellow',
+                    'border': '5px solid rgb(47, 165, 159)',
+                    'border-radius': '12px',
+                });
 
+                setTimeout(() => {
+
+                    $('.aside-contact').css({
+                        'transform': 'scale(1)',
+                        'margin-top': '0px',
+                        'background-color': 'rgba(252, 252, 252, 0.932)',
+                        'border': 'none',
+                    });
+                }, 3400);
+            }
+
+        });
     });
 
-}
+};
 
 
 
