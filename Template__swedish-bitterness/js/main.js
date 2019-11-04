337b743c320295e40613eaf64ee9d6c9c0c4e305
Vitalii Pidkivka <vitaliy.pidkivka@gmail.com>
2019-11-04 18:43:54 +0300

    Finished project swedish bitter

--- Template__swedish-bitterness/js/main.js
+++ Template__swedish-bitterness/js/main.js
@@ -1,17 +1,17 @@
 $(function() {
-  $(".list__inner .tab").on("click", function(event) {
-    let id = $(this).attr("data-id");
-    $(".list__inner")
-      .find(".tab-item")
-      .removeClass("active-tab")
-      .hide();
-    $(".list__inner .tabs")
-      .find(".tab")
-      .removeClass("active");
-    $(this).addClass("active");
-    $("#" + id)
-      .addClass("active-tab")
-      .fadeIn();
-    return false;
-  });
+    $(".list__inner .tab, .reviews__tab").on("click", function(event) {
+        let id = $(this).attr("data-id");
+        $(".list__inner, .reviews__inner")
+            .find(".tab-item, .reviews__content-item")
+            .removeClass("active-tab")
+            .hide();
+        $(".list__inner .tabs, .reviews__tabs")
+            .find(".tab, .reviews__tab")
+            .removeClass("active");
+        $(this).addClass("active");
+        $("#" + id)
+            .addClass("active-tab")
+            .fadeIn();
+        return false;
+    });
 });
